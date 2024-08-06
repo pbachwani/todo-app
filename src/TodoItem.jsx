4c4023e962e9efe8d7e00944fb0569382492bb80
Prateek Bachwani <bachwani.prateek@gmail.com>
2024-08-06 15:44:06 +0530

    first commit

---
+++ src/TodoItem.jsx
@@ -0,0 +1,20 @@
+// eslint-disable-next-line react/prop-types
+const TodoItem = ({ completed, id, title, toggleTodo, deleteTodo }) => {
+  return (
+    <li>
+      <label>
+        <input
+          type="checkbox"
+          checked={completed}
+          onChange={(e) => toggleTodo(id, e.target.checked)}
+        />
+        {title}
+      </label>
+      <button onClick={() => deleteTodo(id)} className="btn btn-danger">
+        Delete
+      </button>
+    </li>
+  );
+};
+
+export default TodoItem;
